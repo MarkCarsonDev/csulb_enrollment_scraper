a2818e1b05a644ff379455e0412c2141e5e4ea38
MarkCarsonDev <markcarson.dev@gmail.com>
2023-12-11 00:50:19 -0800

    Discord bot changes

--- scraper.py
+++ scraper.py
@@ -1,49 +1,44 @@
-# web_scraper.py
-
 import time
+import aiohttp
 import requests
 from bs4 import BeautifulSoup
 import discord
-# .env file for variables
 from dotenv import dotenv_values
+import asyncio
 
+# Load configuration from .env file
 config = dotenv_values(".env")
 
-
+# Discord bot token
 TOKEN = config["DISCORD_TOKEN"]
+TARGET_USER_ID = int(config["TARGET_USER_ID"])
 
+# Initialize Discord bot
 bot = discord.Client(intents=discord.Intents.default())
 
-bot.run(TOKEN)
-
-TARGET_TOKEN = config["TARGET_TOKEN"]
-
-def send_message(userid: int, message=None):
-    user = bot.get_user(userid)
-    message = message or "This Message is sent via send_message"
-    user.send(message)
-
-@bot.event
-async def on_ready():  # this is when the bot gets started, it will call the "on_ready" function
-    print(f'{bot.user} is now running')  # this tells us that our bot is up and ready
-    
-
-def monitor_course_sections(url, course_title, check_interval=60):
+async def send_message(user_id: int, message=None):
     """
-    Monitors the number of sections for a course and alerts if more than 2 sections are found.
+    Asynchronously sends a message to a user on Discord.
 
     Args:
-    url (str): The URL to scrape.
-    course_title (str): The title of the course to monitor.
-    check_interval (int): Time interval in seconds between checks.
+    user_id (int): Discord user ID to whom the message is sent.
+    message (str): Message content. Defaults to a predefined message.
+    """
+    user = await bot.fetch_user(user_id)
+    message = message or "This Message is sent via send_message"
+    await user.send(message)
+    
+
+async def monitor_course_sections(url, course_title, check_interval=60):
+    """
+    Asynchronously monitors the number of sections for a course.
     """
     while True:
-        sections = fetch_course_sections(url, course_title)
+        sections = await fetch_course_sections(url, course_title)
         if len(sections) > 3:
             print("Alert: More than 3 sections found for the course!")
             break
-
-        time.sleep(check_interval)
+        await asyncio.sleep(check_interval)
 
 def get_value_of_key_starting_with(dict_data, start_string):
     # Iterate through each key in the dictionary
@@ -99,22 +94,18 @@ def print_dict(dict_data):
     for key, value in dict_data.items():
         print(f"\t{key}: {value}")
 
-def fetch_course_sections(url, course_title):
+async def fetch_course_sections(url, course_title):
     """
-    Fetches and prints the sections table for a specific course title from the given URL.
-    
-    Args:
-    url (str): The URL to scrape.
-    course_title (str): The title of the course to look for.
+    Asynchronously fetches and prints the sections table for a specific course title from the given URL.
     """
-    # Fetch the HTML content from the URL
-    response = requests.get(url)
-    if response.status_code != 200:
-        print("Failed to retrieve the web page.")
-        return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status != 200:
+                print("Failed to retrieve the web page.")
+                return []
 
-    # Parse the HTML content
-    soup = BeautifulSoup(response.text, 'html.parser')
+            # Parse the HTML content
+            soup = BeautifulSoup(await response.text(), 'html.parser')
 
     # Find the course header div
     course_header = soup.find(lambda tag: tag.name == "div" and tag.get("class", []) == ["courseHeader"] and course_title in tag.text)
@@ -129,10 +120,12 @@ def fetch_course_sections(url, course_title):
                 if section["Open Seats"] > 0:
                     print("Open seats available!")
                     print_dict(section)
-                    send_message(TARGET_TOKEN, f'Open seats available!\n{section["Section"]}\n{section["Class Number"]}\n{section["Instructor"]}\n{section["Open Seats"]}')
+                    await send_message(TARGET_USER_ID, f'Open seats available!\n{section["Section"]}\n{section["Class Number"]}\n{section["Instructor"]}\n{section["Open Seats"]}')
             if open_seats == 0:
                 print(f'No open seats available on {time.strftime("%H:%M:%S")} at {time.strftime("%d/%m/%Y")}')
-                send_message(TARGET_TOKEN, f'No open seats available on {time.strftime("%H:%M:%S")} at {time.strftime("%d/%m/%Y")}')
+                await send_message(TARGET_USER_ID, f'No open seats available on {time.strftime("%H:%M:%S")} at {time.strftime("%d/%m/%Y")}')
+            activity = discord.Activity(type=discord.ActivityType.watching, name=f"{open_seats} seats available (Updated {time.strftime('%H:%M')} on {time.strftime('%A')})")
+            await bot.change_presence(status=discord.Status.idle, activity=activity)
         
                 
         else:
@@ -145,5 +138,12 @@ def fetch_course_sections(url, course_title):
 url = "https://web.csulb.edu/depts/enrollment/registration/class_schedule/Spring_2024/By_Subject/CECS.html"
 course_title = "COMPUTER SCI SENIOR PROJECT I"
 
-# Fetch and display course sections
-monitor_course_sections(url, course_title)
+@bot.event
+async def on_ready():
+    """
+    Called when the bot is ready. Prints bot's username.
+    """
+    print(f'{bot.user} is now running')
+    asyncio.create_task(monitor_course_sections(url, course_title))  # Correctly create a task for monitoring
+
+bot.run(TOKEN)
